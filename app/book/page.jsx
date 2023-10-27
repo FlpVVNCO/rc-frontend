87857b97f9625a79f3455a24453c8819bb35186a
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-27 16:08:54 -0300

    book route

--- app/book/page.jsx
+++ app/book/page.jsx
@@ -3,8 +3,8 @@ import BookList from "../../components/BookList";
 import Paginator from "../../components/Paginator";
 export default function BookListPage() {
   return (
-    <Grid container direction="row" alignItems="center" justifyContent="center">
-      <Grid item xs={12} sm={10} xl={6.1} mt={2} sx={{}}>
+    <Grid container>
+      <Grid item xs={12} sm={10} xl={6.1} mt={2}>
         <BookList />
         <Paginator />
       </Grid>
