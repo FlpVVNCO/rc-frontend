45a4802191ef22fac8459b523ff9810d2937c7f6
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-27 03:18:26 -0300

    bookpage

--- app/books/page.jsx
+++ app/books/page.jsx
@@ -1,20 +1,12 @@
+"use client";
 import { Box, Grid, Toolbar, Typography } from "@mui/material";
 import BookList from "../../components/BookList";
 import FilterList from "../../components/FilterList";
 import Paginator from "../../components/Paginator";
-export default async function Books() {
+export default function Books() {
   return (
     <Grid container direction="row" alignItems="center" justifyContent="center">
-      <Grid
-        item
-        xs={12}
-        sm={10}
-        xl={6.1}
-        mt={2}
-        sx={{
-  
-        }}
-      >
+      <Grid item xs={12} sm={10} xl={6.1} mt={2} sx={{}}>
         <BookList />
         <Paginator />
       </Grid>
