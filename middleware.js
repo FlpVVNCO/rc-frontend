d7c3b01cd0c3420fdbd222c5cad36c85d396ab6d
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-27 04:27:17 -0300

    login

--- middleware.js
+++ middleware.js
@@ -1,22 +1,23 @@
-import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
-export async function middleware(req) {
-  const session = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+// export async function middleware(req) {
+//   const session = await getToken({
+//     req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
 
-  console.log(session?.user.confirmed);
+//   console.log(session?.user.confirmed);
 
-  if (!(session && session.user.confirmed === 1)) {
-    // Si la sesión no existe o el usuario no está confirmado, redirige a la página de inicio de sesión ('/login')
-    const redirectURL = new URL("/login", req.nextUrl.origin);
-    return NextResponse.redirect(redirectURL.href);
-  }
+//   if (!(session && session.user.confirmed === 1)) {
+//     // Si la sesión no existe o el usuario no está confirmado, redirige a la página de inicio de sesión ('/login')
+//     const redirectURL = new URL("/login", req.nextUrl.origin);
+//     return NextResponse.redirect(redirectURL.href);
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 export const config = {
   // Define aquí las rutas que deseas que sean manejadas por el middleware
