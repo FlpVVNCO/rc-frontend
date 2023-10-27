a0a3d6362a98ddb39101a470f0e3800f6badbe0d
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-26 23:00:56 -0300

    fix middleware

--- middleware.js
+++ middleware.js
@@ -1,25 +1,37 @@
+import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(req) {
-  const session = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  if (!session || session.user.confirmed === 0) {
-    console.log("no está confirmado");
-    const requestedPage = req.nextUrl.pathname;
-    const url = req.nextUrl.clone();
-    url.pathname = `/login`;
-    url.search = `p=${requestedPage}`;
-    return NextResponse.redirect(url);
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextUrl.token);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.user.confirmed === 1,
+    },
   }
+);
 
-  console.log("está confirmado");
-  return NextResponse.next();
-}
-// See "Matching Paths" below to learn more
+// export async function middleware(req) {
+//   const session = await getToken({
+//     req,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
+
+//   if (!session || session.user.confirmed === 0) {
+//     console.log("no está confirmado");
+//     const requestedPage = req.nextUrl.pathname;
+//     const url = req.nextUrl.clone();
+//     url.pathname = `/login`;
+//     url.search = `p=${requestedPage}`;
+//     return NextResponse.redirect(url);
+//   }
+
+//   console.log("está confirmado");
+//   return NextResponse.redirect();
+// }
+// // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/books", "/search", "/profile"],
+  matcher: ["/", "/search", "/profile", "/books"],
 };
