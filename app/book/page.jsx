0aa8ecdf941ef84474de8acc8adff08a948778a7
Felipe Vivanco <felipe.vasquez@coredevx.com>
2023-10-27 16:19:10 -0300

    book routes

--- app/book/page.jsx
+++ app/book/page.jsx
@@ -1,6 +1,16 @@
 import { Grid } from "@mui/material";
 import BookList from "../../components/BookList";
 import Paginator from "../../components/Paginator";
-export default function BookListPage() {
-  return <h1>BOOKLIST</h1>;
-}
+
+const BookListPage = () => {
+  return (
+    <Grid container direction="row" alignItems="center" justifyContent="center">
+      <Grid item xs={12} sm={10} xl={6.1} mt={2}>
+        <BookList />
+        <Paginator />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default BookListPage;
